ac13d8e10499b5d656204f2f83f04a22866a3859
nlekkerman <nlekkerman@gmail.com>
2024-02-06 21:33:34 +0000

    check input values added

--- run.py
+++ run.py
@@ -1,3 +1,40 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+CREDS = Credentials.from_service_account_file('credentials.json')
+
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+
+# Corrected method for authentication
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+def get_sales_data():
+    """
+    get sales input from the user
+    """
+
+  
+
+    data_str = input("Enter your data here: ")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+def validate_data(values):
+    """
+    """
+    try:
+        if len(values) != 6:
+          raise ValueError(
+            f"Exactly 6 values required, you provided {len(values)}"
+        )
+    except ValueError as e:
+       print(f"Invalid data:{e}")
+get_sales_data()
