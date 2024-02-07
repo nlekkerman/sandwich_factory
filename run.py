0076cca6cacf28c3d071889293ffc84a9fe93389
nlekkerman <nlekkerman@gmail.com>
2024-02-07 12:14:00 +0000

    Calculate stock

--- run.py
+++ run.py
@@ -79,6 +79,17 @@ def get_last_five_entries_sale():
         column = sales.col_values(ind)
         columns.append(column[-5:])
     return columns
+def calculate_stock_data(data):
+    """Calculate the average stock for each item type"""
+    print("calculating....\n")
+    new_stock_data = []
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    return new_stock_data
+    
 def main():
     """Runn all program function"""
     data = get_sales_data()
@@ -86,7 +97,11 @@ def main():
     update_worsheet(sales_data,"sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worsheet(new_surplus_data,"surplus")
+    sales_column = get_last_five_entries_sale()
+    stock_data = calculate_stock_data(sales_column)
+    update_worsheet(stock_data,"stock")
+
+    print(stock_data)
 print("Welcome to Love Sandwiches Automation")
    
-#main()
-sales_column = get_last_five_entries_sale()
+main()
